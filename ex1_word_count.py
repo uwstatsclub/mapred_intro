acb64114c40d3a8fae004b96676beb946baaf9ea
Jeeyoung Kim <jeeyoungk@gmail.com>
2012-02-08 16:08:08 -0500

    Added normalization function.

--- ex1_word_count.py
+++ ex1_word_count.py
@@ -3,12 +3,18 @@
 
 # This program can be executed by running:
 # python dumbo/cmd.py start ex1_word_count.py -input book_dataset/pg10.txt -input book_dataset/pg1342.txt -output ex1_output
+import re;
+
+def normalize(word):
+  return re.sub('[^a-z]', '', word.lower())
 
 def mapper(key, value):
   # key - line number
   # value - line of text.
   for word in value.split():
-    yield (word, 1)
+    normalized = normalize(word)
+    if normalized:
+      yield (normalized, 1)
 
 def reducer(key, values):
   yield (key, sum(values))
@@ -16,13 +22,14 @@ def reducer(key, values):
 def bigram_mapper(key, values):
   prev_word = None
   for word in values.split():
-    if prev_word is not None:
-      yield ((prev_word, word), 1)
-    prev_word = word
+    cur_word = normalize(word)
+    if prev_word and cur_word:
+      yield ((prev_word, cur_word), 1)
+    prev_word = cur_word
 
 if __name__ == "__main__":
   # Entry point to the program.
   import dumbo
   # dumbo.run(mapper, reducer)
   # run this to get bigram count, instead of word (unigram) count.
-  dumbo.run(bigram_mapper, reducer)
+  # dumbo.run(bigram_mapper, reducer)
